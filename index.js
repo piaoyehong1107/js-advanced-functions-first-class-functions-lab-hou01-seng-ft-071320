e90da715840a8c49c7e162543b87f53bba54fe24
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-28 03:17:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function returnLastTwoDrivers(driver){
 const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
 
 function createFareMultiplier(int){
-  const fareQuintupler=function(fare){
+  const fareQuintupler=function(){
    fare * int
   }
 }
