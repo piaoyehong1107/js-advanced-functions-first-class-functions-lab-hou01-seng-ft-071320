d05845cbb95daeaeecd2157b050bf5104afda24a
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-28 02:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(driver){
+  const myfunc=function(){
+    let array=[]
+    array.push(driver[0],driver[1])
+    return array
+  }
+  return myfunc()
+}
+
+function returnLastTwoDrivers(driver){
+  const myfunc=function(){
+    let array=[]
+    array.push(driver[driver.length-2],driver[driver.length-1])
+    return array
+  }
+  return myfunc()
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+  
